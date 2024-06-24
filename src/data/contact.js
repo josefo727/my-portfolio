cd837c8a23e37eaffd473a384e29926b0a2d6258
José R. Gutierrez <josefo727@gmail.com>
2024-06-24 16:21:45 -0500

    feat: add contact information and link to Vultr articles

--- src/data/contact.js
+++ src/data/contact.js
@@ -1,11 +1,11 @@
 export default {
-  email: 'josefo727@gmail.com',
-  web_site: 'https://jose-gutierrez.com',
-  facebook: 'https://www.facebook.com/joserafael.gutierrezblanco',
-  twitter: 'https://twitter.com/Nige81',
-  skype: 'https://join.skype.com/invite/ByfPA21DyIiR',
-  linkedin: 'https://www.linkedin.com/in/jose-gutierrez-5a2293128/',
-  instagram: 'https://www.instagram.com/josefo1981/',
-  phone: '+576018394169',
-  mobil: '+573219069839',
-}
+  email: "josefo727@gmail.com",
+  web_site: "https://jose-gutierrez.com",
+  facebook: "https://www.facebook.com/joserafael.gutierrezblanco",
+  twitter: "https://twitter.com/Nige81",
+  skype: "https://join.skype.com/invite/ByfPA21DyIiR",
+  linkedin: "https://www.linkedin.com/in/jose-gutierrez-5a2293128/",
+  instagram: "https://www.instagram.com/josefo1981/",
+  phone: "+576018394169",
+  mobil: "+573026178155",
+};
